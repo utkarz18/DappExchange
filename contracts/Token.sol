195b813e61af43a015b99056eee1bb64077b01bd
Utkarsh Pandey <utkarz18@gmail.com>
2024-03-24 01:14:24 +0530

    adding balanceOf function to the token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,17 @@ pragma solidity ^0.8.24;
 import 'hardhat/console.sol';
 
 contract Token {
-    string public name = 'UTK';
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
 }
--- test/token.ts
+++ test/token.ts
@@ -1,11 +1,45 @@
 import { expect } from "chai";
+import { ContractTransactionResponse } from "ethers";
 import { ethers } from "hardhat";
+import { Token } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe('Token', () => {
-    it('has a name', async () => {
-        const tokenContract = await ethers.getContractFactory('Token');
-        let token = await tokenContract.deploy();
-        const tokenName = await token.name();
-        expect(tokenName).equals('UTK');
-    })
+    let token: Token & { deploymentTransaction(): ContractTransactionResponse; };
+    let deployer: HardhatEthersSigner;
+
+    describe('Deployment', () => {
+        const name = 'testToken';
+        const symbol = 'TTK';
+        const decimals = 18;
+        const totalSupply = ethers.parseUnits('1000000', 'ether');
+
+        beforeEach(async () => {
+            const tokenContract = await ethers.getContractFactory('Token');
+            token = await tokenContract.deploy(name, symbol, 1000000);
+
+            const accounts = await ethers.getSigners();
+            deployer = accounts[0];
+        });
+
+        it('has correct name', async () => {
+            expect(await token.name()).equals(name);
+        })
+
+        it('has correct symbol', async () => {
+            expect(await token.symbol()).equals(symbol);
+        })
+
+        it('has correct decimals', async () => {
+            expect(await token.decimals()).equals(decimals);
+        })
+
+        it('has correct total supply', async () => {
+            expect(await token.totalSupply()).equals(totalSupply);
+        })
+
+        it('assign total supply to the deployer', async () => {
+            expect(await token.balanceOf(deployer.address)).equals(totalSupply);
+        })
+    });
 });
