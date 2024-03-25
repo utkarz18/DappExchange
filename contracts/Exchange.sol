145ff230c22d4e5f84929135a71295623f0cda1c
Utkarsh Pandey <utkarz18@gmail.com>
2024-03-25 18:19:52 +0530

    deposit token function + test cases

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,12 +1,31 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+import "./Token.sol";
 
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
 
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address _token, address _user, uint256 _amount, uint256 _balance);
+
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    function depositToken(address _token, uint256 _amount) public {
+        require(Token(_token).transferfrom(msg.sender, address(this), _amount));
+        tokens[_token][msg.sender] += _amount;
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function balanceOf(
+        address _token,
+        address _user
+    ) public view returns (uint256) {
+        return tokens[_token][_user];
     }
 }
--- test/exchange.ts
+++ test/exchange.ts
@@ -11,16 +11,27 @@ const tokens = (value: number) => {
 describe('Exchange', () => {
     let deployer: HardhatEthersSigner;
     let feeAccount: HardhatEthersSigner;
+    let seller: HardhatEthersSigner;
+
     let exchange: Exchange & { deploymentTransaction(): ContractTransactionResponse; };
+    let token1: Token & { deploymentTransaction(): ContractTransactionResponse; };
+
     const feePercent = 10;
 
     beforeEach(async () => {
         const accounts = await ethers.getSigners();
         deployer = accounts[0];
         feeAccount = accounts[1];
-        
+        seller = accounts[2];
+
         const exchangeContract = await ethers.getContractFactory('Exchange');
         exchange = await exchangeContract.deploy(feeAccount, feePercent);
+
+        const tokenConract = await ethers.getContractFactory('Token');
+        token1 = await tokenConract.deploy('Test coin', 'TTK', 1000000);
+
+        const transaction = await token1.connect(deployer).transfer(seller.address, tokens(100));
+        await transaction.wait();
     });
 
     describe('Deployment', () => {
@@ -32,5 +43,36 @@ describe('Exchange', () => {
         it('tracks the fee percent', async () => {
             expect(await exchange.feePercent()).to.be.equal(feePercent);
         })
+    });
+
+    describe('Depositing Tokens', () => {
+        let amount = tokens(100);
+        let result: any;
+
+        describe('Success', () => {
+            beforeEach(async () => {
+                let transaction = await token1.connect(seller).approve(exchange.getAddress(), amount);
+                await transaction.wait();
+    
+                transaction = await exchange.connect(seller).depositToken(token1.getAddress(), amount);
+                result = await transaction.wait();
+            });
+
+            it('tracks the token deposit to exchange', async () => {
+                expect(await exchange.balanceOf(token1.getAddress(), seller.address)).to.be.equal(amount);
+                expect(await token1.balanceOf(exchange.getAddress())).to.be.equal(amount);
+                expect(await token1.balanceOf(seller.address)).to.be.equal(0);
+            })
+
+            it('emits an Deposit event', async () => {
+                expect(await result?.logs[1].fragment.name).to.be.equal('Deposit');
+            })
+        });
+
+        describe('Failure', () => {
+            it('fails when no tokens are approved', async () => {
+                await expect(exchange.connect(seller).depositToken(token1.getAddress(), amount)).to.be.reverted;
+            });
+        });
     });
 });
