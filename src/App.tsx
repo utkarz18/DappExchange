a8fa1dd1ce8496bf6a0122e7cc5de901d1cef3ab
Utkarsh Pandey <utkarz18@gmail.com>
2024-03-27 23:33:06 +0530

    init UI + connecting to metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { Contract, ethers } from 'ethers';
+import './App.css'
+import config from './config.json'
+import TOKEN_ABI from './token_abi.json'
+
 
 const App = () => {
+
+  const loadBlockChainData = async () => {
+    if (window.ethereum == null) {
+      alert("MetaMask not installed; using read-only defaults")
+      return;
+    }
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    console.log(accounts[0]);
+
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const { chainId } = await provider.getNetwork();
+    console.log(chainId);
+
+    const addressOf = (config as any)[chainId.toString()];
+    const MUSDT = new Contract(addressOf.MUSDT, TOKEN_ABI, provider);
+    console.log(`Token fetched:\nSymbol: ${await MUSDT.symbol()} \nAddress: ${await MUSDT.getAddress()}`)
+  }
+  useEffect(() => {
+    loadBlockChainData();
+  })
+
   return (
-    <div>Hello React</div>
-  )
+    <div>
+
+      {/* Navbar */}
+
+      <main className='exchange grid'>
+        <section className='exchange__section--left grid'>
+
+          {/* Markets */}
+
+          {/* Balance */}
+
+          {/* Order */}
+
+        </section>
+        <section className='exchange__section--right grid'>
+
+          {/* PriceChart */}
+
+          {/* Transactions */}
+
+          {/* Trades */}
+
+          {/* OrderBook */}
+
+        </section>
+      </main>
+
+      {/* Alert */}
+
+    </div>
+  );
 }
 
-export default App
+export default App;
